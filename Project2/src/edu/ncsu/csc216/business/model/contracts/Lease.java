3f6cd15fd06e9bab863f5cce9a9a1a8c0212e8a2
maraum <maraum@ncsu.edu>
2020-03-30 16:10:45 -0400

    added throws markers to both Lease constructors

--- Project2/src/edu/ncsu/csc216/business/model/contracts/Lease.java
+++ Project2/src/edu/ncsu/csc216/business/model/contracts/Lease.java
@@ -5,6 +5,8 @@ package edu.ncsu.csc216.business.model.contracts;
 
 import java.time.LocalDate;
 
+import edu.ncsu.csc216.business.model.properties.RentalCapacityException;
+import edu.ncsu.csc216.business.model.properties.RentalDateException;
 import edu.ncsu.csc216.business.model.properties.RentalUnit;
 import edu.ncsu.csc216.business.model.stakeholders.Client;
 
@@ -39,7 +41,7 @@ public class Lease implements Comparable<Lease> {
 	 * @param endDate
 	 * @param i
 	 */
-	public Lease(Client client, RentalUnit unit, LocalDate startDate, LocalDate endDate, int i) {
+	public Lease(Client client, RentalUnit unit, LocalDate startDate, LocalDate endDate, int i) throws RentalCapacityException, RentalDateException {
 		
 	}
 	
@@ -52,7 +54,7 @@ public class Lease implements Comparable<Lease> {
 	 * @param endDate
 	 * @param j
 	 */
-	public Lease(int i, Client client, RentalUnit unit, LocalDate startDate, LocalDate endDate, int j) {
+	public Lease(int i, Client client, RentalUnit unit, LocalDate startDate, LocalDate endDate, int j) throws RentalCapacityException, RentalDateException {
 		
 	}
 	
