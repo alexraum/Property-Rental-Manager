fb4d47dd947504051a38587d966faeb0a59fac86
maraum <maraum@ncsu.edu>
2020-04-10 12:27:16 -0400

    Updated ConferenceRoom constructor

--- Project2/src/edu/ncsu/csc216/business/model/properties/ConferenceRoom.java
+++ Project2/src/edu/ncsu/csc216/business/model/properties/ConferenceRoom.java
@@ -31,7 +31,6 @@ public class ConferenceRoom extends RentalUnit {
 	 */
 	public ConferenceRoom(String location, int capacity) {
 		super(location, capacity);
-		super.takeOutOfService();
 		if (capacity > MAX_CAPACITY) {
 			throw new IllegalArgumentException();
 		}
