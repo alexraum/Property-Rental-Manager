d903b5fcdb8426343d6a413fc87ecf3bf675bddc
waclem <waclem@ncsu.edu>
2020-04-17 13:50:55 -0400

    Change close rental unit to remove service from unit object.

--- Project2/src/edu/ncsu/csc216/business/model/stakeholders/PropertyManager.java
+++ Project2/src/edu/ncsu/csc216/business/model/stakeholders/PropertyManager.java
@@ -244,7 +244,8 @@ public class PropertyManager implements Landlord {
 			throw new IllegalArgumentException();
 		}
 		RentalUnit unit = getUnitAtFilteredIndex(propertyIndex);
-		removeFromService(propertyIndex, EARLIEST_DATE);
+		// removeFromService(propertyIndex, EARLIEST_DATE);
+		unit.removeFromServiceStarting(EARLIEST_DATE);
 		rooms.remove(rooms.indexOf(unit));
 		
 		// TODO Still need to cancel all Leases for the removed RentalUnit
